983ae84ac6e0f64a37ce7bdf182aa5e5cdaf2d75
John Doe <johndoe@example.com>
2020-10-13 14:44:48 +0300

    finish base table logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,14 +1,22 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from '@/components/table/table.template';
 import { resizeHandler } from '@/components/table/table.resize';
-import { shouldResize } from '@/components/table/table.functions';
+import {
+  shouldResize,
+  isCell,
+  matrix,
+  nextSelector } from '@/components/table/table.functions';
+import { TableSelection } from '@/components/table/TableSelection';
+import { $ } from '@core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'table';
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
-      listeners: ['mousedown']
+      name: 'Table',
+      listeners: ['mousedown', 'keydown', 'input'],
+      ...options
     });
   }
 
@@ -16,9 +24,66 @@ export class Table extends ExcelComponent {
     return createTable(20);
   }
 
+  prepare() {
+    this.selection = new TableSelection();
+  }
+
+  init() {
+    super.init();
+
+    this.selectCell(this.$root.find('[data-id="0:0"]'));
+
+    this.$on('formula:input', text => {
+      this.selection.current.text(text);
+    });
+
+    this.$on('formula:done', () => {
+      this.selection.current.focus();
+    });
+  }
+
+  selectCell($cell) {
+    this.selection.select($cell);
+    this.$emit('table:select', $cell);
+  }
+
   onMousedown(evt) {
     if (shouldResize(evt)) {
       resizeHandler(this.$root, evt);
+    } else if (isCell(evt)) {
+      const $target = $(evt.target);
+      if (evt.shiftKey) {
+        const $cells = matrix($target, this.selection.current)
+            .map(id => this.$root.find(`[data-id="${id}"]`));
+
+        this.selection.selectGroup($cells);
+      } else {
+        this.selection.select($target);
+      }
     }
   }
+
+  onKeydown(evt) {
+    const keys = [
+      'Enter',
+      'Tab',
+      'ArrowLeft',
+      'ArrowRight',
+      'ArrowDown',
+      'ArrowUp'
+    ];
+    const {key} = evt;
+
+    if (keys.includes(key) && !evt.shiftKey) {
+      evt.preventDefault();
+      const id = this.selection.current.id(true);
+      const $next = this.$root.find(nextSelector(key, id));
+      this.selectCell($next);
+    }
+  }
+
+  onInput(evt) {
+    this.$emit('table:input', $(evt.target));
+  }
 }
+
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,10 +3,18 @@ const CODES = {
   Z: 90
 };
 
-function toCell(_, col) {
-  return `
-    <div class="cell" contenteditable data-col=${col}></div>
-  `;
+function toCell(row) {
+  return function(_, col) {
+    return `
+      <div
+        class="cell"
+        contenteditable
+        data-type="cell"
+        data-col=${col}
+        data-id=${row}:${col}
+      ></div>
+    `;
+  };
 }
 
 function toColumn(col, index) {
@@ -50,13 +58,13 @@ export function createTable(rowsCount = 31) {
 
   rows.push(createRow(cols));
 
-  for (let i = 1; i <= rowsCount; i++) {
+  for (let row = 1; row <= rowsCount; row++) {
     const cells = new Array(colsCount)
         .fill('')
-        .map(toCell)
+        .map(toCell(row - 1))
         .join('');
 
-    rows.push(createRow(cells, i));
+    rows.push(createRow(cells, row));
   }
 
   return rows.join('');
