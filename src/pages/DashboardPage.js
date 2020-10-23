5977abaa999244729f4941e0fb610054752238db
John Doe <johndoe@example.com>
2020-10-23 21:34:26 +0300

    Routing implemented

---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,29 @@
+import { Page } from '../core/Page';
+import { createRecordsTable } from './dashboard.functions';
+import { $ } from '@core/dom';
+
+
+export class DashbordPage extends Page {
+  getRoot() {
+    const now = Date.now().toString();
+    return $.create('div', 'db').html(
+        `
+        <div class="db__header">
+          <h1>Excel Панель Управления</h1>
+        </div>
+        <div class="db__new">
+          <div class="db__view">
+            <a href="#excel/${now}" class="db__create">
+              Новая <br> таблица
+            </a>
+          </div>
+        </div>
+        <div class="db__table db__view">
+
+          ${createRecordsTable()}
+
+        </div>
+        `
+    );
+  }
+}
